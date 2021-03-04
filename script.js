e889535dcab5291e2a9817e4816267867a8e429e
Isimeri Tori <jake.yogg.saron@gmail.com>
2021-03-04 05:37:31 +0200

    removed scrolling when the nav is open

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const closeBtn = document.querySelector(".close-icon");
 const burgerBtn = document.querySelector(".hamburger-container");
 const mobileNav = document.querySelector(".mobile-nav-container");
+const body = document.querySelector("body");
 
 burgerBtn.addEventListener("click", showNav);
 closeBtn.addEventListener("click", hideNav);
 
 function showNav(){
     mobileNav.classList.add("show");
+    body.classList.add("nav-open");
 }
 function hideNav(){
     mobileNav.classList.remove("show");
+    body.classList.remove("nav-open");
 }
